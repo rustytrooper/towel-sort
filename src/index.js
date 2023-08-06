b2b0d10abffc7ac7d74e9f2874598ac5597262d0
rustytrooper <diansolo40@gmail.com>
2023-08-06 23:10:33 +0300

    init: done task with push against map in local file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let output=[]
+  if (!matrix) {
+    return []
+  }
+  for (let i=0; i<matrix.length; i++){
+    if (i % 2 != 0) {
+      console.log(i);
+      output.push(matrix[i].reverse())
+    } else {
+      output.push(matrix[i])
+    }
+  }
+  let endOutput = output.flat()
+  return endOutput
 }
